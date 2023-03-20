1d5d941652930ba53abb2b6d729a1c1499dc53fd
judemusyoki <judemusyoki@gmail.com>
2023-03-20 02:45:38 +0100

    add hover effect

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -8,22 +8,25 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
       rel="noopener noreferrer"
       className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
     >
-      <img
-        src={imgUrl}
-        alt="portfolio"
-        className="w-full h-36 md:h-48 object-cover cursor-pointer"
-      />
-      <div className="w-full p-4">
-        <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">
-          {title}
-        </h3>
-        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
-          {stack.map((item) => (
-            <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
-              {item}
-            </span>
-          ))}
-        </p>
+      <div className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
+        <img
+          src={imgUrl}
+          alt="portfolio"
+          className="w-full h-36 md:h-48 object-cover cursor-pointer max-w-xs transition duration-300 ease-in-out hover:scale-110"
+        />
+
+        <div className="w-full p-4">
+          <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">
+            {title}
+          </h3>
+          <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
+            {stack.map((item) => (
+              <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
+                {item}
+              </span>
+            ))}
+          </p>
+        </div>
       </div>
     </a>
   )
